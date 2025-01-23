230daf4919496b8da1806ff3bbcc9b25f768fa0c
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-23 17:58:03 +0100

    feat: :white_check_mark: create CountryModel

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -9,4 +9,27 @@ class Country extends Model
 {
     /** @use HasFactory<\Database\Factories\CountryFactory> */
     use HasFactory;
+
+    protected $table = 'country';
+    protected $primaryKey = 'Code';
+    protected $incrementing = false;
+    protected $keyType = 'string';
+    protected $timestamps = false;
+    protected $fillable = [
+        'Code',
+        'Name',
+        'Continent',
+        'Region',
+        'SurfaceArea',
+        'IndepYear',
+        'Population',
+        'LifeExpectancy',
+        'GNP',
+        'GNPOld',
+        'LocalName',
+        'GovernmentForm',
+        'HeadOfState',
+        'Capital',
+        'Code2'
+    ];
 }
