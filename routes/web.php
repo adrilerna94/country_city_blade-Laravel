19eb8bdf96b20f223b3a4a71689c65e219d944e4
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-23 18:12:09 +0100

    feat: :zap: Adding Route::resource in web.php routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CountryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::resource a diferencia de Route::apiResource inclou create y edit methods
+Route::resource('countries', CountryController::class);
