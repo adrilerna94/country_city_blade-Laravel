c78ee10794e9514804ff7dd23ce86af1f2fbb362
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-23 18:37:18 +0100

    feat: :white_check_mark: CountryController done

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -11,7 +11,7 @@ class CountryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         $countries = Country::all();
         return response()->json($countries, 200);
@@ -21,48 +21,63 @@ class CountryController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        // return view('countries.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCountryRequest $request)
+    public function store(StoreCountryRequest $request): RedirectResponse
     {
-        //
+        // antes debería pasar middleware de validación
+        $country = Country::create($request->all());
+        return response()->json($country, 201);
+        // return redirect()->route('countries.index')
+        //                  ->with('success', 'Country created sucessfully.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Country $country)
+    public function show(Country $country): View
     {
-        //
+        $country = Country::findOrFail($country->Code);
+        return response()->json($country, 200);
+        // return view('countries.show', compact('country'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Country $country)
+    public function edit(Country $country): View
     {
-        //
+        // return view('countries.edit', compact('country'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCountryRequest $request, Country $country)
+    public function update(UpdateCountryRequest $request, Country $country): RedirectResponse
     {
-        //
+        // Previamente validado en el middleware
+        $updatedCountry = $country->update($request->all());
+        return response()->json($updatedCountry, 200);
+        /* return redirect()->route('countries.index')
+                            ->with('success', 'Product updated successfully');
+        */
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Country $country)
+    public function destroy(Country $country): RedirectResponse
     {
-        //
+        $country->delete();
+        return response()->noContent();
+        /* return redirect()->route('countries.index')
+                            ->with('success', 'Country deleted successfully');
+        */
     }
 }
