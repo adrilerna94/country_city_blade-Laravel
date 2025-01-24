c590cdb59f31fc923cba9a9cc4b52cb1f9b1a003
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-24 20:51:59 +0100

    feat: :white_check_mark: creating city model with relation with city

--- app/Models/City.php
+++ app/Models/City.php
@@ -9,4 +9,49 @@ class City extends Model
 {
     /** @use HasFactory<\Database\Factories\CityFactory> */
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'city'; // Per defecte la taula es diu igual que el model en plural i "snake_case".
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'ID'; // Per defecte busca la columna "id".
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false; // Per defecte és true.
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+      'ID',
+      'Name',
+      'CountryCode',
+      'District',
+      'Population'
+    ];
+
+    /**
+     * Get the country that has the city.
+     * 1st param: Model
+     * 2nd param: Foreign key
+     * 3rd param: Primary Key of the related table
+     */
+    public function country()
+    {
+        return $this->belongsTo(Country::class, 'CountryCode', 'Code');
+    }
 }
