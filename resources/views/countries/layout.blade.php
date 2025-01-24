7205da1e740d05d5afd81acce4270412e0b83acb
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-24 19:32:18 +0100

    feat: :art: layout created

---
+++ resources/views/countries/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Country CRUD</title>
+</head>
+<body>
+    <header>
+        <h1>COUNTRIES</h1>
+    </header>
+    <div class="container">
+        @yield('content')  <!-- Aquí se inyectará el contenido de cada vista -->
+    </div>
+</body>
+</html>
